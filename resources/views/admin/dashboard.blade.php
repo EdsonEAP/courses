0f93eabfba65cc514293fca5ac374ca58a1c705c
EdsonEAP <edsonap2001@gmail.com>
2024-09-03 14:19:21 -0500

    cambios uwu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,34 @@
 <!-- resources/views/some-view.blade.php -->
-<x-admin-layout>
+<x-admin-layout :breadcrumb="[
+    [
+        'name' => 'Dashboard',
+        'url' => '#',
+    ],
+    [
+        'name' => 'Dynamic Content',
+        'url' => '#',
+    ],
+]">
+
+
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
+    <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
     <p>Este es el contenido dinámico que se mostrará en el área 'dynamic-content'.</p>
 </x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,5 @@
+@props(['breadcrumb' => []])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -10,25 +12,40 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    <!-- Fonts awesone-->
+    <script src="https://kit.fontawesome.com/63dda6191f.js" crossorigin="anonymous"></script>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
     @livewireStyles
 </head>
-<body>
-
+<body x-data="{
+        open: false
+    }"
+    :class="{
+        'overflow-hidden': open
+    }">
+    class = "sm:overflow-auto"
 
     @include('layouts.includes.admin.navigation')
     @include('layouts.includes.admin.sidebard')
 
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-            {{ $slot }}
+        <div class="mt-14">
+            @include('layouts.includes.admin.breadcrumb')
+            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                {{ $slot }}
+            </div>
         </div>
     </div>
 
+    <div
+        style="display: none"
+        x-show="open"
+        x-on:click="open = false"
+        class="bg-gray-900 bg-opacity-50 fixed inset-0 z-30 sm:hidden">
+    </div>
     @stack('modals')
 
     @livewireScripts
